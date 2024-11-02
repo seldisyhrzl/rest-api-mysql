02e69b156d02e485f4e6862cd24e6ef1d0c9ba3f
seldisyhrzl <seldysz114@gmail.com>
2024-11-02 22:48:58 +0700

    setup environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "tutor_dek",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
 });
 
 module.exports = dbPool.promise();
